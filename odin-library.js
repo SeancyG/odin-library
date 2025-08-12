646b4b55773525140dd2d220bf662bb13df7fff0
SeancyG <164585793+SeancyG@users.noreply.github.com>
2025-08-11 21:59:44 -0700

    Add dialog with form submission.

--- odin-library.js
+++ odin-library.js
@@ -1,15 +1,24 @@
 const myLibrary = [];
 
-function Book(title) {
+const dialog = document.querySelector("dialog");
+const addBookButton = document.querySelector(".add-book");
+addBookButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+const bookForm = document.querySelector("form");
+bookForm.addEventListener("submit", submitBook);
+
+function Book(title, readStatus) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor.")
     }
     this.id = crypto.randomUUID();
     this.title = title;
+    this.readStatus = readStatus;
 }
 
-function addBookToLibrary(title) {
-    const newBook = new Book(title);
+function addBookToLibrary(title, readStatus) {
+    const newBook = new Book(title, readStatus);
     myLibrary.push(newBook);
     displayBook(newBook);
 }
@@ -24,12 +33,29 @@ function displayBook(book) {
     title.classList.add("title");
     title.textContent = book.title;
     div.appendChild(title);
+    const readStatus = document.createElement("input");
+    //readStatus.setAttribute("checked")
 }
 
 function populateBookshelf() {
     for (let index = 0; index < myLibrary.length; index++) {
         displayBook(myLibrary[index]);
     }
+}
+
+function submitBook(event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    const title = formData.get("title");
+    const readStatusForm = formData.get("readStatus");
+    let readStatus;
+    if(readStatusForm) {
+        readStatus = true;
+    } else {
+        readStatus = false;
+    }
+
+    addBookToLibrary(title, readStatus);
 }
 
 populateBookshelf();
