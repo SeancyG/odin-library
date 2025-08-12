4e0c236a684900edaabb2f40252adf64f0f09e7f
SeancyG <164585793+SeancyG@users.noreply.github.com>
2025-08-11 19:32:48 -0700

    Create basic add book functionality and display on page.

--- odin-library.js
+++ odin-library.js
@@ -8,6 +8,28 @@ function Book(title) {
     this.title = title;
 }
 
-function addBookToLibrary(Book) {
-
+function addBookToLibrary(title) {
+    const newBook = new Book(title);
+    myLibrary.push(newBook);
+    displayBook(newBook);
 }
+
+function displayBook(book) {
+    const bookshelf = document.querySelector(".bookshelf");
+    const div = document.createElement("div");
+    div.classList.add("book");
+    div.setAttribute("id", book.id);
+    bookshelf.appendChild(div);
+    const title = document.createElement("p");
+    title.classList.add("title");
+    title.textContent = book.title;
+    div.appendChild(title);
+}
+
+function populateBookshelf() {
+    for (let index = 0; index < myLibrary.length; index++) {
+        displayBook(myLibrary[index]);
+    }
+}
+
+populateBookshelf();
