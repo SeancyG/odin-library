af07284c349ae3fe9e82989d00d39a243ad1e301
SeancyG <164585793+SeancyG@users.noreply.github.com>
2025-08-11 23:23:36 -0700

    Add remove book and change read status functionality.

--- odin-library.js
+++ odin-library.js
@@ -7,6 +7,21 @@ addBookButton.addEventListener("click", () => {
 });
 const bookForm = document.querySelector("form");
 bookForm.addEventListener("submit", submitBook);
+const removeButton = document.querySelector(".remove");
+const bookshelf = document.querySelector(".bookshelf");
+bookshelf.addEventListener("click", (e) => {
+    let target = e.target;
+    if(e.target.classList.contains("remove")) {
+        removeBook(e.target.dataset.id);
+    } else if(e.target.type=="checkbox") {
+        if(e.target.checked == true) {
+            changeReadStatus(myLibrary[myLibrary.findIndex(item => item.id === e.target.dataset.id)], true);
+            console.log(true);
+        } else {
+            changeReadStatus(myLibrary[myLibrary.findIndex(item => item.id === e.target.dataset.id)], false);
+        }
+    }
+})
 
 function Book(title, readStatus) {
     if (!new.target) {
@@ -27,14 +42,26 @@ function displayBook(book) {
     const bookshelf = document.querySelector(".bookshelf");
     const div = document.createElement("div");
     div.classList.add("book");
-    div.setAttribute("id", book.id);
+    div.setAttribute("data-id", book.id);
     bookshelf.appendChild(div);
     const title = document.createElement("p");
     title.classList.add("title");
     title.textContent = book.title;
     div.appendChild(title);
     const readStatus = document.createElement("input");
-    //readStatus.setAttribute("checked")
+    readStatus.setAttribute("type", "checkbox");
+    readStatus.setAttribute("data-id", book.id);
+    if(book.readStatus) {
+        readStatus.checked = true;
+    } else {
+        readStatus.checked = false;
+    }
+    div.appendChild(readStatus);
+    const remove = document.createElement("button");
+    remove.classList.add("remove");
+    remove.setAttribute("data-id", book.id);
+    remove.textContent = "Remove";
+    div.appendChild(remove);
 }
 
 function populateBookshelf() {
@@ -56,6 +83,17 @@ function submitBook(event) {
     }
 
     addBookToLibrary(title, readStatus);
+}
+
+function removeBook(id) {
+    const book = document.querySelector(`div[data-id="${id}"]`)
+    console.log(book);
+    bookshelf.removeChild(book);
+    myLibrary.splice(myLibrary.findIndex(item => item.id === id), 1);
+}
+
+function changeReadStatus(book, readStatus) {
+    book.readStatus = readStatus;
 }
 
 populateBookshelf();
